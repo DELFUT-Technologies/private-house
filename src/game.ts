e495bc97bd1a985eb7c60161a281328fe467d494
yuh-dot <xue11pow0@gmail.com>
2022-08-26 01:07:46 +0900

    model change

--- src/game.ts
+++ src/game.ts
@@ -1,7 +1,7 @@
 // Create a main house
 const house = new Entity()
 
-house.addComponent(new GLTFShape('models/KameHouse_0813.glb'))
+house.addComponent(new GLTFShape('models/modify_ryugujo_Open0824.glb'))
 house.addComponent(
   new Transform({
     position: new Vector3(0, 0, 0),
@@ -13,7 +13,7 @@ engine.addEntity(house)
 // Create a wall
 const wall = new Entity()
 
-wall.addComponent(new GLTFShape('models/外壁3_small.glb'))
+wall.addComponent(new GLTFShape('models/外壁3_small0823.glb'))
 wall.addComponent(
   new Transform({
     position: new Vector3(-3, 0, -2),

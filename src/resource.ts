68433acc295b3ef33135b73cd2facea2e4309765
AuroraSloan <lanajadethompson@gmail.com>
2023-02-26 20:02:42 +0900

    fixlinterr

--- src/resource.ts
+++ src/resource.ts
@@ -1,8 +1,8 @@
 export const Parent = new Entity()
 Parent.addComponent(
-    new Transform({
-        position: new Vector3(0, 0, 16),
-        rotation: Quaternion.Euler(0, 90, 0)
-    })
+  new Transform({
+    position: new Vector3(0, 0, 16),
+    rotation: Quaternion.Euler(0, 90, 0)
+  })
 )
 engine.addEntity(Parent)

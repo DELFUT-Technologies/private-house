a453078d624ec0e4747c977826a4ba50fc8199a5
AuroraSloan <lanajadethompson@gmail.com>
2023-04-17 23:47:59 +0900

    fix kamechi convo

--- src/talkingKamecchi.ts
+++ src/talkingKamecchi.ts
@@ -1,15 +1,28 @@
-import { NPC, Dialog } from "@dcl/npc-scene-utils"
+import { NPC, NPCDelay, Dialog } from "@dcl/npc-scene-utils"
 
+let Kamecchi: NPC
+const animations: string[] = ['[うなづく]', '[かしげる]', '[笑う]', '[首を振る]']
+
+function randAnimTrigger(max: number) {
+    const num = Math.floor(Math.random()*max);
+
+    Kamecchi.playAnimation(animations[num], true)
+}
+
+function animTrigger(index: number) {
+    Kamecchi.playAnimation(animations[index], true)
+}
+//
 // chatGPTが間に合わなかった場合のバックアップダイアログ
 const kamecchiDialog: Dialog[] = [
     { // 0
         text: "Welcome! I'm glad you could make it. Please, make yourself at home. What can I get for you?",
         isQuestion: true,
         buttons: [
-            {label: 'おすすめは', goToDialog: 1},
-            {label: 'ハイボール', goToDialog: 2},
-            {label: 'ビール', goToDialog: 2},
-            {label: '梅酒', goToDialog: 2}
+            {label: 'おすすめは', goToDialog: 1, triggeredActions: () => animTrigger(2)},
+            {label: 'ハイボール', goToDialog: 2, triggeredActions: () => animTrigger(2)},
+            {label: 'ビール', goToDialog: 2, triggeredActions: () => animTrigger(2)},
+            {label: '梅酒', goToDialog: 2, triggeredActions: () => animTrigger(2)}
         ]
     },
     { // 1
@@ -17,18 +30,18 @@ const kamecchiDialog: Dialog[] = [
         isQuestion: true,
         fontSize: 20,
         buttons: [
-            {label: 'Yona Yona', goToDialog: 2},
-            {label: 'ビール', goToDialog: 2},
-            {label: 'ハイボール', goToDialog: 2},
-            {label: '梅酒', goToDialog: 2},
+            {label: 'Yona Yona', goToDialog: 2, triggeredActions: () => animTrigger(1)},
+            {label: 'ビール', goToDialog: 2, triggeredActions: () => animTrigger(1)},
+            {label: 'ハイボール', goToDialog: 2, triggeredActions: () => animTrigger(1)},
+            {label: '梅酒', goToDialog: 2, triggeredActions: () => animTrigger(1)},
         ]
     },
     { // 2
         text: 'Coming right up...',
         isQuestion: true,
         buttons: [
-            {label: 'ありがとう', goToDialog: 5},
-            {label: '作る動機は?', goToDialog: 3},
+            {label: 'ありがとう', goToDialog: 5, triggeredActions: () => randAnimTrigger(2)},
+            {label: '作る動機は?', goToDialog: 3, triggeredActions: () => randAnimTrigger(3)},
         ]
     },
     { // 3
@@ -36,8 +49,8 @@ const kamecchiDialog: Dialog[] = [
         isQuestion: true,
         fontSize: 18,
         buttons: [
-            {label: 'なるほど', goToDialog: 5},
-            {label: 'メタ社未は?', goToDialog: 4},
+            {label: 'なるほど', goToDialog: 5, triggeredActions: () => randAnimTrigger(2)},
+            {label: 'メタ社未は?', goToDialog: 4, triggeredActions: () => randAnimTrigger(3)},
         ]
     }, 
     { // 4
@@ -45,42 +58,51 @@ const kamecchiDialog: Dialog[] = [
         isQuestion: true,
         fontSize: 20,
         buttons: [
-            {label: "ありがとう!", goToDialog: 5},
-            {label: "またね!", goToDialog: 5},
+            {label: "ありがとう!", goToDialog: 5, triggeredActions: () => randAnimTrigger(2)},
+            {label: "またね!", goToDialog: 5, triggeredActions: () => randAnimTrigger(2)},
         ]
     },
     { // 5
         text: 'I hope to see you again soon.',
-        isEndOfDialog: true
+        isEndOfDialog: true,
+        triggeredByNext: () => {
+            animTrigger(1)
+        }
     }
-  ]
+] 
 
 export function InitKamecchi() {
-    new NPC(
+    const dummyent = new Entity()
+    engine.addEntity(dummyent)
+    dummyent.addComponent(
+        new NPCDelay(2, () => {
+            Kamecchi.playAnimation(animations[0])
+        })
+    )
+
+    Kamecchi = new NPC(
         {
-            position: new Vector3(11.46, 10.63, 11.82),
+            position: new Vector3(11.46, 10.70, 11.82),
             rotation: Quaternion.Euler(0, 270, 0)
         },
-        // TODO: Get bar model from haji
-        'models/bar_kamecchi_2023.04.15_2126.glb',
+        'models/bar_kamecchi_2023.04.17_1128.glb',
         () => {
-            // TODO: アニメーションをランダム化する
-            // [うなづく、かしげる、笑う、首を振る]
-            // Kamecchi.playAnimation('Hello', false, 2)
 
+            animTrigger(1)
 
             // TODO: You-San ユーザーから質問を受けるための入力UIを開く
 
-
             // chatGPTが間に合わなかった場合のバックアップダイアログ
-            // Kamecchi.talk(kamecchiDialog)
+            Kamecchi.talk(kamecchiDialog)
         },
         {
             faceUser: false,
             darkUI: true,
             hoverText: 'Order a drink',
             onlyClickTrigger: true,
-            onWalkAway: () => {}
+            onWalkAway: () => {
+                Kamecchi.endInteraction()
+            }
         }
     )
 }
